16d8a51abc1a0eafb927fb95b708bd1918738117
Aurélien Bombo <abombo@microsoft.com>
2024-03-28 20:10:03 +0000

    deploy: refer to "cc-local" name instead

--- pkg/azureconstants/azure_constants.go
+++ pkg/azureconstants/azure_constants.go
@@ -26,7 +26,7 @@ const (
 	DefaultAzureCredentialFileEnv     = "AZURE_CREDENTIAL_FILE"
 	DefaultCredFilePathLinux          = "/etc/kubernetes/azure.json"
 	DefaultCredFilePathWindows        = "C:\\k\\azure.json"
-	DefaultDriverName                 = "disk.csi.azure.com"
+	DefaultDriverName                 = "cc.local.csi.azure.com"
 	DesIDField                        = "diskencryptionsetid"
 	DiskEncryptionTypeField           = "diskencryptiontype"
 	DiskAccessIDField                 = "diskaccessid"
